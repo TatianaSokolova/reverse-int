e1bb107ecb7bbe170ed94487323e54d41c4ad1b7
TatianaSokolova <Stanya0427@gmail.com>
2020-10-13 19:05:17 +0300

    add decision

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
-  
+    let arr = Math.abs(n).toString().split('');
+    if (arr[arr.length - 1] == 0) {
+      arr.pop();
+    } else {
+      arr;
+    }
+    return +arr.reverse().join('');
 }
